c6ac5636bcae73ffde6861eeda9f0c1fc369a536
udhaya <udhayakumarpandiyan@gmail.com>
2019-12-17 16:52:27 +0530

    Updates

---
+++ precache-manifest.189f8426b7a0dbc9b9ef6333585d0836.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a4ad362f2dd448fbf7c47a6e33b37f",
+    "url": "/carttask/index.html"
+  },
+  {
+    "revision": "31c6524efe1b6adb654b",
+    "url": "/carttask/static/css/2.4d221211.chunk.css"
+  },
+  {
+    "revision": "b078f66ca02eb5752a3c",
+    "url": "/carttask/static/css/4.27a25997.chunk.css"
+  },
+  {
+    "revision": "a7c69843f5235b02b93f",
+    "url": "/carttask/static/css/main.d5caf913.chunk.css"
+  },
+  {
+    "revision": "31c6524efe1b6adb654b",
+    "url": "/carttask/static/js/2.3442b4f4.chunk.js"
+  },
+  {
+    "revision": "2a9e3c1ea42e182e32f0",
+    "url": "/carttask/static/js/3.f0c6322d.chunk.js"
+  },
+  {
+    "revision": "b078f66ca02eb5752a3c",
+    "url": "/carttask/static/js/4.5958da78.chunk.js"
+  },
+  {
+    "revision": "a7c69843f5235b02b93f",
+    "url": "/carttask/static/js/main.36e751ff.chunk.js"
+  },
+  {
+    "revision": "1865bae9a7d27659d336",
+    "url": "/carttask/static/js/runtime~main.19265da8.js"
+  },
+  {
+    "revision": "b7180c84eb001f261d3b853ef6a8f19a",
+    "url": "/carttask/static/media/Verdana.b7180c84.woff"
+  },
+  {
+    "revision": "ba34b303291e36596759eb46ad9c51f2",
+    "url": "/carttask/static/media/verdana.ba34b303.ttf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/carttask/precache-manifest.189f8426b7a0dbc9b9ef6333585d0836.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/carttask/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
