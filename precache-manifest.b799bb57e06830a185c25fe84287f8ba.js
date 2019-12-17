3dbe57b5c2405c7e9c5d229ff5c68fb60ab3983c
udhaya <udhayakumarpandiyan@gmail.com>
2019-12-17 18:39:34 +0530

    Updates

--- precache-manifest.b799bb57e06830a185c25fe84287f8ba.js
+++ precache-manifest.b799bb57e06830a185c25fe84287f8ba.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c922e8227815d3ea02f253efbd9f5072",
+    "revision": "3f51042d59666f1376024996fddda64d",
     "url": "/cart-task/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cart-task/static/css/2.4d221211.chunk.css"
   },
   {
-    "revision": "4ee49631a94e8b40d9f7",
+    "revision": "b121732034a8ab0aac65",
     "url": "/cart-task/static/css/4.a22a7d7f.chunk.css"
   },
   {
@@ -24,16 +24,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cart-task/static/js/3.6a9b6454.chunk.js"
   },
   {
-    "revision": "4ee49631a94e8b40d9f7",
-    "url": "/cart-task/static/js/4.5678be23.chunk.js"
+    "revision": "b121732034a8ab0aac65",
+    "url": "/cart-task/static/js/4.e42d8828.chunk.js"
   },
   {
     "revision": "833da351d320ef39f451",
     "url": "/cart-task/static/js/main.8a41a778.chunk.js"
   },
   {
-    "revision": "49d64ac100432122a8b2",
-    "url": "/cart-task/static/js/runtime~main.f5034d64.js"
+    "revision": "4bfd71050d318b4bdc2e",
+    "url": "/cart-task/static/js/runtime~main.aa77b9b9.js"
   },
   {
     "revision": "b7180c84eb001f261d3b853ef6a8f19a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cart-task/precache-manifest.c4035ccede45e51b19800b58185335c2.js"
+  "/cart-task/precache-manifest.b799bb57e06830a185c25fe84287f8ba.js"
 );
 
 self.addEventListener('message', (event) => {
